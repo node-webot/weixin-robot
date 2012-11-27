38fb1e565f844232dd3c9ed6717acef2de8ea46a
ktmud <jyyjcc@gmail.com>
2012-11-27 09:44:46 +0800

    more comment

--- examples/routes.js
+++ examples/routes.js
@@ -1,15 +1,22 @@
-// Special type for location
 var weixin = require('../lib/weixin');
 var router = weixin.router();
 var geo2loc = weixin.geo2loc;
+
 var dialogs = weixin.dialogs({
   dir: __dirname + '/' + 'dialogs',
   files: ['basic', 'greetings.js', 'gags']
 });
-
+// dialogs 就是一个Array
+// 类似于：
+// [
+//   ['你好', '你也好'],
+//   ['我叫(.*)', '你好，\1']
+// ]
 router.dialog(dialogs);
 
+// Special type for location
 router.set('location', function(info, next) {
+  // 已经提供了geo转地址的工具，使用的是高德地图的API
   geo2loc(info.param, function(loc_info) {
     if (!loc_info) return '我不知道你在什么地方。';
     return '你正在' + loc_info['addr'];
