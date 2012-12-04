787529a3dd09d4f46ef20336842ffa63b0df7db7
ktmud <jyyjcc@gmail.com>
2012-12-04 14:16:38 +0800

    more annotations

--- examples/app.js
+++ examples/app.js
@@ -5,7 +5,16 @@ var debug = require('debug');
 var log = debug('wx');
 
 var robot = require('../lib/robot')(require('./routes'), require('./waits'));
-var weixin = require('../lib/weixin');
+var webot = require('../lib/weixin');
+
+// 不建议在正式环境中这样用
+// 请安装 npm package `weixin-robot`，
+// 然后：
+//  var webot = require('weixin-robot');
+//  var router = webot.router();
+//  var waiter = webot.waiter();
+//  var robot = webot.robot(router, waiter);
+
 var messages = {
   '400': '听不懂你在说什么哦',
   '503': '服务器临时出了一点问题，您稍后再来好吗'
@@ -19,7 +28,7 @@ var app = express();
 app.enable('trust proxy');
 
 // 检查请求权限的 middleware
-var checkSig = weixin.checkSig(WX_TOKEN);
+var checkSig = webot.checkSig(WX_TOKEN);
 
 app.get('/', checkSig);
 
@@ -28,7 +37,7 @@ app.get('/', checkSig);
 // {
 //   'keepBlank': false // 是否保留消息头尾的空白字符，默认为 undefined
 // }
-app.post('/', checkSig, weixin.bodyParser(), function(req, res, next) {
+app.post('/', checkSig, webot.bodyParser(), function(req, res, next) {
   var info = req.wx_data;
 
   // 返回给微信的，必须是一个 xml
@@ -38,7 +47,7 @@ app.post('/', checkSig, weixin.bodyParser(), function(req, res, next) {
     // 返回消息
     // 如果 info.items 为一个数组，则发送图文消息
     // 否则按 info.reply 发送文字消息
-    res.send(weixin.makeMessage(info));
+    res.send(webot.makeMessage(info));
   }
 
   if (!info) {
@@ -53,6 +62,7 @@ app.post('/', checkSig, weixin.bodyParser(), function(req, res, next) {
       //res.statusCode = (typeof err === 'number' ? err : 500);
       info.reply = ret || messages[String(err)] || messages['503'];
     } else if (ret instanceof Array) {
+      // 在 app 层决定如何处理 robot 返回的内容
       info.items = ret;
     } else if (typeof ret == 'string') {
       info.reply = ret;
@@ -62,6 +72,16 @@ app.post('/', checkSig, weixin.bodyParser(), function(req, res, next) {
     end();
   });
 });
+
+// 图文列表的属性对应关系
+// 有时候你返回给 webot.makeMessage 的 items 列表，
+// 里面的对象并不使用标准键值，然后又不想自己用 map 处理
+webot.set('article props', {
+  'pic': 'image',
+  'url': 'uri',
+  'desc': 'description',
+});
+
 
 var port = process.env.PORT || 3000;
 var hostname = '127.0.0.1';
--- examples/routes.js
+++ examples/routes.js
@@ -14,7 +14,7 @@ var dialogs = weixin.dialogs({
 // ]
 router.dialog(dialogs);
 
-// Special type for location
+// 'location' 是保留字，用作用户发送位置的情况
 router.set('location', function(info, next) {
   // 已经提供了geo转地址的工具，使用的是高德地图的API
   geo2loc(info.param, function(loc_info) {
@@ -22,5 +22,55 @@ router.set('location', function(info, next) {
     return '你正在' + loc_info['addr'];
   });
 });
+
+// 为每个route命名，暂时只是为了好看
+router.set('say_hi', {
+  'pattern': /^Hi/i,
+  'handler': function(info, next) {
+    // 如果给传入的 request info 标记 ended，
+    // 则不会进去下一个route（如果有的话）
+    info.ended = true;
+    next(null, '你也好哈');
+  }
+});
+
+var reg_search_cmd = /^(搜索|search)\s*(.+)/i
+router.set('search', {
+  'pattern': reg_search_cmd,
+  'parser': function(info) {
+    // 匹配到的关键词
+    info.q = info.text.match(reg_search_cmd)[2];
+  },
+  // parser 也是可以异步的，error 可以忽略，
+  //'parser': function(info, next) {
+  //  SomeQueryToDatabase(function(err, ret) {
+  //    // err 可直接忽略，如果确已出错，
+  //    // 可以指定:
+  //    //  info.ended = true;
+  //    //  info.err = '获取数据出错..';
+  //    info._data = ret;
+  //    return next(info);
+  //  });
+  //},
+  'handler': function(info, next) {
+    // 从某个地方搜索到数据...
+    do_search({ q: info.q }, function(err, items) {
+      // 给 reques info 里面添加图文列表所需内容
+      next(null, items);
+    });
+  }
+});
+
+// 怎么搜索，自己实现吧...
+function do_search(param, next) {
+  // code...
+  var ret = [{
+    pic: 'http://img.xxx....',
+    url: 'http://....',
+    title: '这个搜索结果是这样的'
+    desc: '哈哈哈哈哈....'
+  }];
+  next(null, ret);
+}
 
 module.exports = router;
