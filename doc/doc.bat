9704f01f49f932355cb4b23c5eacaaef46f127cb
TZ <atian25@qq.com>
2013-01-16 10:57:43 +0800

    增加文档导出的jsduck bat
    Signed-off-by: TZ <atian25@qq.com>

---
+++ doc/doc.bat
@@ -0,0 +1,2 @@
+echo download jsduck from https://github.com/senchalabs/jsduck
+jsduck.exe --title "Weixin Robot - https://github.com/ktmud/weixin-robot" --welcome="../README.md" --output"./tmp/" ../lib/ 
